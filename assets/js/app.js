3e563f2f0fb78a9be6124b9fe55ad4c5fe2f5649
rghossi <rafael.ghossi@gmail.com>
2016-09-08 03:05:33 -0300

    now user can submit forms with enter key

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,15 @@
 var app = angular.module('sctiApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'UserAreaCtrl', 'HeaderCtrl', 'LoginCtrl', 'SignupCtrl']);
+
+app.directive('myEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.myEnter);
+                });
+
+                event.preventDefault();
+            }
+        });
+    };
+});
