918ef5df654ee64967636a4ccbb773c2ddd8fdc8
rghossi <rafael.ghossi@gmail.com>
2016-09-23 11:45:08 -0300

    added createdAt field in users table

--- assets/js/controllers/admin.js
+++ assets/js/controllers/admin.js
@@ -4,7 +4,14 @@ angular.module('AdminCtrl', []).controller('adminCtrl', function($scope, $http) 
 	$http.get('api/users').then(function(res){
 		console.log(res);
 		$scope.users = res.data.users;
+
+
 	}, function(err) {
 		console.log(err);
 	});
+
+	$scope.createdAt = function(id){
+		var timestamp = id.toString().substring(0,8);
+		return new Date( parseInt( timestamp, 16 ) * 1000 ).toLocaleString("pt-BR");
+	}
 });
