0792caf604cab1e1d9b073be116f322ad09d95af
rghossi <rafael.ghossi@gmail.com>
2016-10-18 20:56:40 -0200

    added workshops visualization to admin route

--- assets/js/controllers/admin.js
+++ assets/js/controllers/admin.js
@@ -1,14 +1,30 @@
 var app = angular.module('sctiAppAdmin', ['AdminCtrl']);
 
 angular.module('AdminCtrl', []).controller('adminCtrl', function($scope, $http) {
+	$scope.option = "Workshops";
+	var errCallback = function(err){
+		console.log(err);
+	}
+
+	function hasPaid(user) {
+		return user.hasPayed;
+	}
 
 	$http.get('api/users').then(function(res){
-		console.log(res);
 		$scope.users = res.data.users;
-
-	}, function(err) {
-		console.log(err);
-	});
+		var paidUsers = $scope.users.filter(hasPaid);
+		$http.get('api/workshops/').then(function(res){
+			$scope.workshops = res.data.workshops;
+			$scope.workshops.forEach(function(ws){
+				ws.users = [];
+				paidUsers.forEach(function(user){
+					if (ws.enrolled.includes(user._id)){
+						ws.users.push(user);
+					}
+				});
+			});
+		}, errCallback);
+	}, errCallback);
 
 	$scope.createdAt = function(id){
 		var timestamp = id.toString().substring(0,8);
